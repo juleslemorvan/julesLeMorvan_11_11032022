4682c5f3976c51f6d35a1b373a255c7870f476ec
jules <jules.lemorvan01@gmail.com>
2022-03-12 19:06:05 +0100

    correction card css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,12 @@ import React from "react";
 
 export const Card = ({ title, cover, onClick }) => {
   return (
-    <div className="Card-Container">
-      <div
-        style={{ backgroundImage: `url(${cover})` }}
-        onClick={onClick}
-        className="card"
-      >
-        <h1 className="cardTitle">{title}</h1>
-      </div>
+    <div
+      style={{ backgroundImage: `url(${cover})` }}
+      onClick={onClick}
+      className="card"
+    >
+      <h1 className="cardTitle">{title}</h1>
     </div>
   );
 };
